2ea501f4d1c340463da32bf00841c038453f9174
Paudzno1 <pau30012002@gmail.com>
2023-11-10 09:46:54 +0700

    add_register

--- CinemaSystem/CinemaSystemWPF/View/Login.xaml.cs
+++ CinemaSystem/CinemaSystemWPF/View/Login.xaml.cs
@@ -1,42 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using CinemaSystemLibrary.DataAccess;
 
 namespace CinemaSystemWPF.View
 {
-    /// <summary>
-    /// Interaction logic for Login.xaml
-    /// </summary>
     public partial class Login : Window
     {
+        private readonly CinemaSystemContext _context;
+
         public Login()
         {
             InitializeComponent();
+            _context = new CinemaSystemContext();
         }
 
-        private void btnAddCountry_Click(object sender, RoutedEventArgs e)
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
             string username = txtUsername.Text;
-            string password = txtPassword.Password.ToString();
-            if (username.Equals("admin") && password.Equals("12345") ) {
+            string password = txtPassword.Password;
+
+            // Query the database to check if the user exists
+            var user = _context.Users.FirstOrDefault(u => u.Username == username);
+
+            if (user != null && VerifyPassword(password, user.Password))
+            {
+                // Password is correct, login successful
                 MenuForm menu = new MenuForm();
                 this.Visibility = Visibility.Hidden;
                 menu.Show();
             }
             else
             {
+                // User not found or incorrect password
                 MessageBox.Show("Login fail");
             }
+        }
+
+        private bool VerifyPassword(string enteredPassword, string storedPassword)
+        {
+            // Convert the stored password from base64 to bytes
+            byte[] storedPasswordBytes = Convert.FromBase64String(storedPassword);
+
+            // Extract the salt from the stored password
+            byte[] salt = new byte[16];
+            Array.Copy(storedPasswordBytes, 0, salt, 0, 16);
+
+            // Hash the entered password using the stored salt
+            var pbkdf2 = new Rfc2898DeriveBytes(enteredPassword, salt, 10000, HashAlgorithmName.SHA256);
+            byte[] enteredPasswordHash = pbkdf2.GetBytes(20);
+
+            // Compare the stored hash with the computed hash
+            for (int i = 0; i < 20; i++)
+            {
+                if (enteredPasswordHash[i] != storedPasswordBytes[i + 16])
+                {
+                    return false; // Passwords do not match
+                }
+            }
+
+            return true; // Passwords match
+        }
+
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            Register register = new Register();
+            register.Show();
+            this.Close();
         }
     }
 }
---
+++ CinemaSystem/CinemaSystemWPF/View/Register.xaml.cs
@@ -0,0 +1,129 @@
+﻿// Register.xaml.cs
+using System;
+using System.Windows;
+using CinemaSystemLibrary.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Security.Cryptography;
+
+
+namespace CinemaSystemWPF.View
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        private readonly CinemaSystemContext _context; // Assuming your DbContext is named CinemaSystemContext
+
+        public Register()
+        {
+            InitializeComponent();
+            _context = new CinemaSystemContext(); // Initialize your DbContext
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            // Validation logic
+            if (string.IsNullOrEmpty(txtUsername.Text) || string.IsNullOrEmpty(txtNewPassword.Password))
+            {
+                MessageBox.Show("Username and New Password are required fields.", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Additional validation for Name
+            if (string.IsNullOrEmpty(txtName.Text))
+            {
+                MessageBox.Show("Name is a required field.", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Additional validation for PhoneNumber
+            if (string.IsNullOrEmpty(txtPhoneNumber.Text))
+            {
+                MessageBox.Show("Phone Number is a required field.", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Additional validation for Password complexity
+            if (txtNewPassword.Password.Length < 8)
+            {
+                MessageBox.Show("Password must be at least 8 characters long.", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Check if the username already exists
+            if (_context.Users.Any(u => u.Username == txtUsername.Text))
+            {
+                MessageBox.Show("Username already exists. Please choose a different one.", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            try
+            {
+                // Hash the password
+                string hashedPassword = HashPassword(txtNewPassword.Password);
+
+                // Create a new User object with the registration data
+                var newUser = new User
+                {
+                    Username = txtUsername.Text,
+                    Password = hashedPassword,
+                    Name = txtName.Text,
+                    PhoneNumber = txtPhoneNumber.Text
+                };
+
+                // Add the new user to the database
+                _context.Users.Add(newUser);
+
+                // Save changes to the database
+                _context.SaveChanges();
+
+                // Your registration logic goes here
+
+                MessageBox.Show("Registration successful!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                // Close the current window
+                Login login = new Login();
+                login.Show();
+                this.Close();
+
+                
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error registering user: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private string HashPassword(string password)
+        {
+            // Generate a random salt
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+            // Create a new Rfc2898DeriveBytes object and hash the password with the salt
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000, HashAlgorithmName.SHA256);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            // Combine the salt and password hash
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            // Convert the combined salt and hash to a base64-encoded string
+            string hashedPassword = Convert.ToBase64String(hashBytes);
+
+            return hashedPassword;
+        }
+
+        private void BackToLogin_Click(object sender, RoutedEventArgs e)
+        {
+            // Navigate back to the login window
+            Login login = new Login();
+            login.Show();
+            this.Close(); // Close the current window
+        }
+    }
+}
