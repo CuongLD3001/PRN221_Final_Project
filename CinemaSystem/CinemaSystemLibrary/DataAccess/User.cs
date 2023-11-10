695f2add15817e0b02bac503d9ba32bf10b3f774
hachan140 <havu14102@gmail.com>
2023-11-10 11:46:04 +0700

    fix register

--- CinemaSystem/CinemaSystemLibrary/DataAccess/User.cs
+++ CinemaSystem/CinemaSystemLibrary/DataAccess/User.cs
@@ -5,7 +5,7 @@ namespace CinemaSystemLibrary.DataAccess
 {
     public partial class User
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Username { get; set; } = null!;
         public string Password { get; set; } = null!;
         public string? Name { get; set; }
